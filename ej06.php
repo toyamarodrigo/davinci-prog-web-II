f76da2292bd1982485ee8ce0319e8baeb6c7804c
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-16 04:39:30 -0300

    need fix ej06

--- ej06.php
+++ ej06.php
@@ -16,6 +16,18 @@
     n3<input type="text" name="n[]"><br><br>
     n4<input type="text" name="n[]"><br><br>
     n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
+    n5<input type="text" name="n[]"><br><br>
 
     <input type="submit" name="submit" value="operar">
   </form>
@@ -29,17 +41,34 @@
 $n = array();
 
 if (isset($_POST['submit'])) {
-
   foreach ($_POST['n'] as $num) {
-
     $n[] = $num;
-    $c = count($n);
-
+    $arrlength = count($n);
   }
+  echo "<pre>", print_r($n, 1), "</pre>";
 
-  echo "<pre>", print_r($n,1), "</pre>";
+  for ($i = 0; $i < $arrlength; $i++) {
+    if ($n[$i] == 1) {
 
+      echo $n[$i] . " 1 no es primo<br>";
+    } elseif ($n[$i] == 2) {
 
+      echo $n[$i] . " 2 es primo<br>";
+    } elseif ($n[$i] % 2 == 0) {
+
+      echo $n[$i] . " no es primo<br>";
+    } else {
+
+      for ($j = 3; $j <= ceil(sqrt($n[$i])); $j = $j + 2) {
+        if ($n[$i] % $j == 0) {
+          echo $n[$i] . " 3no es primo<br>";
+        }
+      }
+      if ($n[$i] % $j != 0) {
+        echo $n[$i] . " Aes primo<br>";
+      }
+    }
+  }
 }
 
 
